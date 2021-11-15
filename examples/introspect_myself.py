c381cb83fc887e8dcdd3a553d39cd22487d9c940
AndresBena19 <andres_felipe16@hotmail.es>
2021-11-15 14:37:48 -0500

    feat: update code examples

--- examples/introspect_myself.py
+++ examples/introspect_myself.py
@@ -1,25 +1,38 @@
-"""Example to introspect the same file that execution the introspection"""
+"""Example to introspect the same file that is execution the introspection"""
+import logging
+
 from py_imports.manager import PyImports
 
+from .parse_local_dir import file_imports_from_dir_one
 
-MYSELF = "introspect_myself.py"
+
+MYSELF = "main.py"
 
 # Let's introspect myself
 with PyImports() as manager:
-    manager.get_imports(MYSELF)
-    imports = manager.imports_resume()
+    imports_file = manager.get_imports(MYSELF)
 
 
-# Now yuo have access to the import used in each file
-print(imports)
-
-# Get details about the absolute, relative and standard import in the file
-collector_object = imports.get(MYSELF)
+# Get details about the absolute, relative and standard imports in the file
+collector_object = imports_file.get(MYSELF)
 absolute_imports = collector_object.absolute_imports
 relative_imports = collector_object.relative_imports
-imports = collector_object.imports
+standard_imports = collector_object.imports
 
-# It's obvious that in this file, there are just one absolute import
-# from py_imports.manager import PyImports
+# Absolute imports
+#  --- from py_imports.manager import PyImports ---
 # If we introspect the object, we will get the next
-print(absolute_imports[0])
+example_abs_import = absolute_imports[0]
+
+
+# relative imports
+#  --- from .parse_local_dir import file_imports_from_dir_one ---
+# If we introspect the object, we will get the next
+example_relative_import = relative_imports[0]
+
+# standard imports
+#  --- import logging ---
+# If we introspect the object, we will get the next
+example_standard_import = standard_imports[0]
+
+# Now you know more about you...
